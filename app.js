06b2e2c5b2c5d84b7a02bd0d4d3bd198672d7054
Elogeek <elodiechristin@gmail.com>
2021-01-19 10:26:37 +0100

    la matrice

---
+++ app.js
@@ -0,0 +1,22 @@
+$("button").click(function (){
+    $("div").not(".global").not("#matrice").not(".ligne").toggleClass("plein");
+})
+
+$(".circle").click(function (){
+    $(this).toggleClass("plein");
+})
+
+$("div").not(".global").not("#matrice").not(".ligne").not(".circle").click(function (){
+    $(this).toggleClass("plein");
+    console.log($(this).attr("class"));
+    Array.from($(this).parent().children().not(":not('.circle')")).forEach(elem => {
+        if($(this).attr("class") !== ""){
+            if(!elem.className.includes("plein")){
+                elem.className += " plein";
+            }
+        }
+        else{
+            elem.classList.remove("plein")
+        }
+    });
+});
